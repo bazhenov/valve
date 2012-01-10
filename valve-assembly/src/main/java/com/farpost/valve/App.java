3953a5b3fb43d1191f51c973c820e31b2b19f4d6
Denis Bazhenov <bazhenov@farpost.com>
2012-01-10 19:26:47 +1100

    assembly module added

--- valve-assembly/src/main/java/com/farpost/valve/App.java
+++ valve-assembly/src/main/java/com/farpost/valve/App.java
@@ -1,13 +1,18 @@
-package com.farpost.netty;
+package com.farpost.valve;
+
+import com.farpost.netty.Proxy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.InetSocketAddress;
 
-/**
- * Hello world!
- */
 public class App {
 
+	private static final Logger log = LoggerFactory.getLogger(App.class);
+
 	public static void main(String[] args) {
+		log.info("Running proxy");
+
 		Proxy proxy = new Proxy();
 		proxy.addRemote(new InetSocketAddress("localhost", 8081));
 		proxy.addRemote(new InetSocketAddress("localhost", 8082));
