74d667ac21ebffb6e8af53ecccd63513236cf2fc
Denis Bazhenov <bazhenov@farpost.com>
2012-02-01 10:53:02 +1100

    initializing remote list from console parameters

--- valve-assembly/src/main/java/com/farpost/valve/App.java
+++ valve-assembly/src/main/java/com/farpost/valve/App.java
@@ -6,17 +6,23 @@ import org.slf4j.LoggerFactory;
 
 import java.net.InetSocketAddress;
 
+import static java.lang.Integer.parseInt;
+
 public class App {
 
 	private static final Logger log = LoggerFactory.getLogger(App.class);
 
 	public static void main(String[] args) {
-		log.info("Running proxy");
+		int port = 8080;
 
 		Proxy proxy = new Proxy();
-		proxy.addRemote(new InetSocketAddress("localhost", 8081));
-		proxy.addRemote(new InetSocketAddress("localhost", 8082));
-		proxy.addRemote(new InetSocketAddress("localhost", 8083));
+		proxy.setPort(port);
+		for (String host : args) {
+			String parts[] = host.split(":");
+			log.info("Running proxy on port {}", port);
+			proxy.addRemote(new InetSocketAddress(parts[0], parseInt(parts[1])));
+		}
+		log.info("Running proxy on port {}", port);
 		proxy.run();
 	}
 }
